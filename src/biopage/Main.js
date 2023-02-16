f22269cff6cbeaa0ab72afd4292073ce8a5a9fdb
RickyOdus <odusanyaraphael@gmail.com>
2023-02-16 13:59:59 +0100

    final

--- src/biopage/Main.js
+++ src/biopage/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 export default function Main(){
     return(
         <div>
-<p className="text-2xl text-gray-100 mt-5 ml-8 font-black font-opensans">About</p>
-<p className="text-gray-200 px-6 pl-8"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur maiores
+<p className="About">About</p>
+<p className="Aboutbody"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur maiores
     magni laboriosam nam nobis pariatur? Eligendi atque reiciendis perferendis
     quibusdam provident consectetur.</p>
 
- <p className="text-2xl text-gray-100 mt-4 ml-8 font-black font-opensans">Interest</p>
-<p className="text-gray-200 mb-7 pl-8">  At exercitationem, sequi beatae accusantium
+ <p className="Interest">Interest</p>
+<p className="Interestbody">  At exercitationem, sequi beatae accusantium
     vitae vero explicabo praesentium inventore eius quam! Cumque, officiis fugit
     porro inventore consequatur quis quae laudantium totam, natus obcaecati
     velit beatae, labore molestiae.</p>
