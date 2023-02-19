4bbbf3c4bea29552d2421ff98f2334ec546ec1a0
RickyOdus <odusanyaraphael@gmail.com>
2023-02-19 21:55:27 +0100

    airbnb

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Headerr from './biopage/Headerr';
-import Main from './biopage/Main';
-import Footer from './biopage/Footer';
+// import Headerr from './biopage/Headerr';
+// import Main from './biopage/Main';
+// import Footer from './biopage/Footer';
+
+import Navbar from './airbnb/navbar';
+import Hero from './airbnb/hero';
+import Bottom from './airbnb/bottom';
+
 
 export default function Headed() { //this becomes the Parent component, while these 3 components inside here become the child component
    return (
          <div className="body">
       <div>
-       <Headerr/>
-       <Main/>
-       <Footer/>
+       <Navbar/>
+       <Hero/>
+       <Bottom/>
     </div>
     </div>  
    )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FaHeart } from 'react-icons/fa';
+// import { FaHeart } from 'react-icons/fa';
 import Headed from './App';
-import "./index.css";
+import "./airbnb.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Headed />
+    <Headed/>
   </React.StrictMode>
 );
